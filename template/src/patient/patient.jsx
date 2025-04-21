4035ac29de4f45825c2ca43c6986c93d9bf3fc0d
balsem <balsem.khouni@esprit.tn>
2025-04-21 20:32:05 +0100

    Added missing Navigation component for nurse module

--- template/src/patient/patient.jsx
+++ template/src/patient/patient.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Navigation } from "./Navigation";
-import { Homepage } from "./homepage";
-import { ListeSpecialites } from "./specialite";
-import { Contact } from "./contact";
-import { Complaint } from "./Complaint";
+// Try importing both ways to ensure compatibility
+import Navigation from './Navigation';
+// import { Navigation } from './Navigation'; 
 
 export default function Patient() {
   return (
     <div>
       <Navigation />
-      <Homepage /> {/* Appel de Homepage */}
-      <ListeSpecialites />{/* List des specialites */ }
-      <Complaint></Complaint>{/* Complaint */ }   
-      <Contact></Contact>{/* Contact */ }
     </div>
   );
 }
