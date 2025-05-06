6770657932bdd4bffdb0cb1afc3ca6974d00458b
balsem <balsem.khouni@esprit.tn>
2025-05-06 01:26:41 +0100

    Removed embedded Git repositories from tracking

--- server/models/AmbulanceStaff.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const AmbulanceStaffSchema = new mongoose.Schema({
-    contactNumber: Number
-});
-
-const AmbulanceStaffModel = mongoose.model("ambulanceStaff", AmbulanceStaffSchema);
-module.exports = AmbulanceStaffModel;
--- server/models/Nurse.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const NurseSchema = new mongoose.Schema({
-    bloc: String
-});
-
-const NurseModel = mongoose.model("nurses", NurseSchema);
-module.exports = NurseModel;
